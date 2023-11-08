cf55452d07b388bc2145754020919a75658ea70e
gtsixery <Germancerratejr@gmail.com>
2023-11-08 10:22:30 -0500

    - updated unguided.html

--- javascript.js
+++ javascript.js
@@ -1,19 +1,53 @@
 let slideIndex = 0;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
 showSlides();
+//const timeout = setTimeout(showSlides, 7000); // Change image every 7 seconds
+let timer;
+
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {slideIndex = 1}
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 10000); // Change image every 10 seconds
+  slideTimer();
+
 }
+
+function slideTimer(){
+  setTimeout(showSlides, 7000);
+}
+
+function getSlide(slideIndex){
+  let k;
+
+  for (k = 0; k < slides.length; k++) {
+    slides[k].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (k = 0; k < dots.length; k++) {
+    dots[k].className = dots[k].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
+
+
+function changeSlide(slideIndex) {
+  getSlide(slideIndex);
+  clearTimeout(timer);
+
+
+}
+
